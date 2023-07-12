891375ab5e3f0b2e04777901cdad98cf5f293653
仁志 <legend1world@gmail.com>
2023-07-12 17:35:41 +0900

    Update marker_color.js

--- leaflet/marker_color/marker_color.js
+++ leaflet/marker_color/marker_color.js
@@ -10,11 +10,107 @@ let red = (feature, latlng) => {
 	});
 }
 
+let orange = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FFA500",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let yellow = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FFFF00",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let lightblue = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#00FFFF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
 let blue = (feature, latlng) => {
 
 	return L.circleMarker(latlng, {
 		radius: 10,
 		fillColor: "#0000FF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let purple = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#CC00FF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let green = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#008000",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let lightgreen = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#90EE90",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let black = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#000000",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let white = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FFFFFF",
 		color: "#FFFFFF",
 		weight: 2,
 		opacity: 1,
