c677b371c02289753bdc59ec3755e5d696d71256
仁志 <legend1world@gmail.com>
2023-07-12 17:30:23 +0900

    up

--- leaflet/marker_color/marker_color.js
+++ leaflet/marker_color/marker_color.js
@@ -1,4 +1,16 @@
-let iro = (feature, latlng) => {
+let red = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FF0000",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+let blue = (feature, latlng) => {
 
 	return L.circleMarker(latlng, {
 		radius: 10,
