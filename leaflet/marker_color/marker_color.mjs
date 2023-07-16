241106cc7ed1e81156e87cd82c02ea4e785e0946
仁志 <legend1world@gmail.com>
2023-07-16 19:27:24 +0900

    up

---
+++ leaflet/marker_color/marker_color.mjs
@@ -0,0 +1,119 @@
+export let red = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FF0000",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let orange = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FFA500",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let yellow = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FFFF00",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let lightblue = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#00FFFF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let blue = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#0000FF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let purple = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#CC00FF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let green = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#008000",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let lightgreen = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#90EE90",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let black = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#000000",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
+
+export let white = (feature, latlng) => {
+
+	return L.circleMarker(latlng, {
+		radius: 10,
+		fillColor: "#FFFFFF",
+		color: "#FFFFFF",
+		weight: 2,
+		opacity: 1,
+		fillOpacity: 1
+	});
+}
